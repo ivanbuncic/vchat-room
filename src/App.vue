b63be37ce45997c879e2773f04e8b24e34791148
Ivan Buncic <ivan@triplsi.com>
2021-06-25 16:18:03 +0200

    added some

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+#app {
+  @apply container max-w-4xl;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="max-w-screen-sm">
+    <h1 class="text-purple-700 text-6xl">Hello Kittens!</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
