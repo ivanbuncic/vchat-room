f3483785152f4277c592ef44d3e99947aef728c9
Ivan Buncic <ivan@triplsi.com>
2021-06-26 18:35:50 +0200

    adde one

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="max-w-screen-sm">
-    <h1 class="text-purple-700 text-6xl">Hello Kittens!</h1>
+  <div
+    class="w-screen h-screen grid content-center items-center border-red-400 border-8"
+  >
+    <div class="max-w-screen-sm mx-auto border-4 border-gray-300 border-double">
+      <h1 class="text-purple-700 text-6xl text-center p-5">Hello Kittens!</h1>
+      <p class="mt-6 mb-4 p-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim hic qui
+        alias iure nobis eligendi corrupti provident unde illo eaque autem...
+      </p>
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   purge: ["./public/index.html", "./src/**/*.{js,vue}"],
-  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
