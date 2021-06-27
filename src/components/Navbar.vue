7ace635a9cb62c0e658842329cf3e78fc1aaa6a2
Ivan Buncic <ivan@triplsi.com>
2021-06-27 11:27:53 +0200

    yap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -56,27 +56,8 @@
               >
                 <MenuItem v-slot="{ active }">
                   <a
-                    href="#"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Your Profile</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Settings</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
+                    @click="handleClick"
+                    class="cursor-pointer"
                     :class="[
                       active ? 'bg-gray-100' : '',
                       'block px-4 py-2 text-sm text-gray-700',
@@ -107,22 +88,7 @@
         <a
           href="#"
           class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >Dashboard</a
-        >
-        <a
-          href="#"
-          class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >Team</a
-        >
-        <a
-          href="#"
-          class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >Projects</a
-        >
-        <a
-          href="#"
-          class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >Calendar</a
+          >Chatroom</a
         >
       </div>
       <div class="pt-4 pb-3 border-t border-gray-200">
@@ -147,21 +113,11 @@
         </div>
         <div class="mt-3 space-y-1">
           <a
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-            >Your Profile</a
-          >
-          <a
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-            >Settings</a
-          >
-          <a
             @click="handleClick"
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-            >Sign out</a
+            class="cursor-pointer block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
           >
+            Sign out
+          </a>
         </div>
       </div>
     </DisclosurePanel>
