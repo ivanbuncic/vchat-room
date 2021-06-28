f166150d2fa1c02fa221990c7fed8cbf989d23cf
Ivan Buncic <ivan@triplsi.com>
2021-06-28 17:56:49 +0200

    added date formatter

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="w-full h-full bg-red-50 rounded-lg mb-3 shadow-lg">
+  <div
+    v-if="formattedDocuments"
+    class="w-full h-full bg-red-50 rounded-lg mb-3 shadow-lg"
+  >
     <p
       v-if="error"
       class="mt-5 p-3 border-red-600 border rounded-md text-red-500 text-sm text-center max-w-max mx-auto"
     >
       {{ error }}
     </p>
-    <div class="p-3" v-if="documents">
+    <div class="p-3" v-if="formattedDocuments">
       <div
         class="rounded-lg mb-5 border border-gray-400 p-3"
-        v-for="doc in documents"
+        v-for="doc in formattedDocuments"
         :key="doc.id"
       >
-        <span class="p-1 text-xs">{{ doc.createdAt.toDate() }}</span
+        <span class="p-1 text-xs">{{ doc.createdAt }}</span
         ><br />
         <span class="p-1 font-semibold text-lg">{{ doc.name }} :</span>
         <span class="p-1 font-thin text-xl">{{ doc.message }}</span>
@@ -23,10 +26,22 @@
 
 <script>
 import getCollection from '../composables/getCollection';
+import { computed } from 'vue';
+import { formatDistanceToNow } from 'date-fns';
+
 export default {
   setup() {
     const { error, documents } = getCollection('messages');
-    return { error, documents };
+    const formattedDocuments = computed(() => {
+      if (documents.value) {
+        return documents.value.map(doc => {
+          let time = formatDistanceToNow(doc.createdAt.toDate());
+          return { ...doc, createdAt: time };
+        });
+      }
+    });
+
+    return { error, documents, formattedDocuments };
   },
 };
 </script>
